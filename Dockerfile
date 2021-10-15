3fd33eebb8df986a4095ac126ec64896c624d5c8
selmo-lyz <37548493+selmo-lyz@users.noreply.github.com>
2021-10-15 10:24:12 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,4 @@ RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 EXPOSE 5000
-COPY ./web/ .
 CMD ["flask", "run"]
--- web/app.py
+++ web/app.py
@@ -3,5 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return render_template('home.html')
+
+@app.route("/research")
+def research():
+    return render_template('research.html')
+
+@app.route("/people")
+def people():
+    return render_template('people.html')
+
+@app.route("/publication")
+def publication():
+    return render_template('publication.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
